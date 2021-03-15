c3bda5a2e7140fedabb9d8870803b34aa971ce4e
mohamed fayez <mf429866@gmail.com>
2021-03-15 23:15:01 +0200

    seed data

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Product;
+use Faker\Generator as Faker;
+use Illuminate\Support\Arr;
+
+$products=[
+    "product1",
+    "product2",
+    "product3",
+    "product4",
+    "product5",
+    "product6",
+    "product7",
+    "product8",
+    "product9",
+    "product10",
+];
+$factory->define(Product::class, function (Faker $faker) use($products) {
+    return [
+        "title" => Arr::random($products),
+        "description" => $faker->text(),
+    ];
+});
